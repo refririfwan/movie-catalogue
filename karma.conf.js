759a3b5fe1ecd39cf5e78508133d0f4386575db5
Refri Rifwan <refririfwan@gmail.com>
2021-07-07 08:54:21 +0700

    Kutangkap Bugs Dengan Jaring Pengaman

--- karma.conf.js
+++ karma.conf.js
@@ -13,6 +13,7 @@ module.exports = function (config) {
     // list of files / patterns to load in the browser
     files: [
       'specs/**/*Spec.js',
+      'specs/likeMovieSpec.js'
     ],
 
     // list of files / patterns to exclude
